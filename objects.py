c29bf81b2a7ebb26cc6846129c6d399d9929f743
nvchung599 <Alaska2001!>
2020-01-12 20:34:31 -0800

    4th commit

--- objects.py
+++ objects.py
@@ -11,24 +11,26 @@ class Player(object):
         self.image = pygame.image.load(os.path.join('images', PLAYER_IMAGE_FILE)).convert_alpha()
         self.game_display = game_display
         self.rect = self.image.get_rect()
-        self.angle = IMAGE_START_ANGLE
-#        self.angle = random.random() * 360
+#        self.angle = IMAGE_START_ANGLE
+        self.angle = random.random() * 360
         self.pos = np.array([x, y])
         self.alliance = alliance
         self.fitness = 0
         self.net = Net(NUM_INPUTS, NUM_HIDDEN_1, NUM_HIDDEN_2, NUM_OUTPUTS)
+        self.soak_time = TARGET_SOAK_TIME
 
         self.image_ch = pygame.image.load(os.path.join('images', 'crosshair.png')).convert_alpha()
 
     def draw(self):
 
-        rotated = pygame.transform.rotate(self.image_ch, self.angle)
-        rot_rect = rotated.get_rect()
-        self.game_display.blit(rotated, (self.pos[0]-rot_rect.center[0], self.pos[1]-rot_rect.center[1]))
+        if DISPLAY_GRAPHICS_BOOL:
+            rotated = pygame.transform.rotate(self.image_ch, self.angle)
+            rot_rect = rotated.get_rect()
+            self.game_display.blit(rotated, (self.pos[0]-rot_rect.center[0], self.pos[1]-rot_rect.center[1]))
 
-        rotated = pygame.transform.rotate(self.image, self.angle)
-        rot_rect = rotated.get_rect()
-        self.game_display.blit(rotated, (self.pos[0]-rot_rect.center[0], self.pos[1]-rot_rect.center[1]))
+#            rotated = pygame.transform.rotate(self.image, self.angle)
+#            rot_rect = rotated.get_rect()
+#            self.game_display.blit(rotated, (self.pos[0]-rot_rect.center[0], self.pos[1]-rot_rect.center[1]))
 
 
     def rotate(self, dt, direction):
@@ -66,8 +68,7 @@ class Player(object):
         return inputs
 
     def reset(self):
-#        self.angle = random.random() * 360
-        self.angle = 0
+        self.angle = random.random() * 360
         self.fitness = 0
 
 
@@ -82,13 +83,15 @@ class Target(object):
 
 
     def draw(self):
-        self.game_display.blit(self.image, (self.pos[0]-self.rect.center[0], self.pos[1]-self.rect.center[1]))
+        if DISPLAY_GRAPHICS_BOOL:
+            self.game_display.blit(self.image, (self.pos[0]-self.rect.center[0], self.pos[1]-self.rect.center[1]))
 
     def update(self, dt):
         self.cooldown_timer -= dt
         if self.cooldown_timer < 0:
             self.pos[0] = random.random() * DISPLAY_W
-            self.pos[1] = random.random() * DISPLAY_H * 0.1 + (DISPLAY_H * 0.9)
+            self.pos[1] = random.random() * DISPLAY_H
+#            self.pos[1] = random.random() * DISPLAY_H * 0.1 + (DISPLAY_H * 0.9)
             self.cooldown_timer = TARGET_COOLDOWN_TIME
         self.draw()
 
@@ -99,20 +102,27 @@ class Collection(object):
         self.game_display = game_display
         self.players = []
         for i in range(GENERATION_POPULATION):
-            self.players.append(Player(game_display, i+1, DISPLAY_W / 2, DISPLAY_H / 2))
+            self.players.append(Player(game_display, i+AI_ONLY_BOOL, DISPLAY_W / 2, DISPLAY_H / 2))
 
     def reset(self):
         for p in self.players:
             p.reset()
 
     def update(self, dt, target, keys):
+
         for player in self.players:
+
             if player.alliance == 0:
                 player.update_manual(dt, keys)
             else:
                 player.update_auto(dt, target)
-            fitness_increment = 1 - (get_angle(player, target) / 180)
-            if fitness_increment > 0.5:
+
+            if get_angle(player, target) < TARGET_GOOD_ANGLE_CONE:
+                player.soak_time -= dt
+            else:
+                player.soak_time = TARGET_SOAK_TIME
+
+            if player.soak_time < 0:
                 player.fitness += 1 - (get_angle(player, target) / 180)
 
     def breed(self, parent_1, parent_2):
@@ -120,17 +130,26 @@ class Collection(object):
         child_player.net.create_mixed_weights(parent_1.net, parent_2.net)
         return child_player
 
+    def amoeba(self, parent):
+        child_player = Player(self.game_display, 0, DISPLAY_W/2, DISPLAY_H/2)
+        child_player.net.create_mixed_weights(parent.net, parent.net)
+        child_player.net.modify_weights()
+        return child_player
+
+
     def evolve(self):
+
         self.players.sort(key=lambda x: x.fitness, reverse=True)
 
-        print('---------------------------')
-        for p in self.players:
-            print(p.fitness)
+        survive_cut_off = int(len(self.players) * SURVIVE_CUT_OFF_PERC)
 
-        cut_off = int(len(self.players) * MUTATION_CUT_OFF)
-        good_players = self.players[0:cut_off]
-        bad_players = self.players[cut_off:]
+        good_players = self.players[0:survive_cut_off]
+        bad_players = self.players[survive_cut_off:]
         num_bad_take = int(len(self.players) * MUTATION_BAD_TO_KEEP)
+
+        print('---------------------------')
+        for p in self.players[:10]:
+            print(p.fitness)
 
         for b in bad_players:
             b.net.modify_weights()
@@ -147,16 +166,59 @@ class Collection(object):
         num_children_needed = len(self.players) - len(new_players)
 
         while len(new_players) < len(self.players):
-            idx_pair_breed = np.random.choice(np.arange(len(good_players)), 2, replace=False)
-            if idx_pair_breed[0] != idx_pair_breed[1]:
-                child = self.breed(good_players[idx_pair_breed[0]], good_players[idx_pair_breed[1]])
-#                if random.random() < MUTATION_WEIGHT_MODIFY_CHANCE:
-#                    child.net.modify_weights()
+            for i in range(3):
+                child = self.amoeba(good_players[i])
                 new_players.append(child)
 
         self.players = new_players
-
         self.reset()
+
+
+#    def evolve(self):
+#
+#        self.players.sort(key=lambda x: x.fitness, reverse=True)
+#
+#        survive_cut_off = int(len(self.players) * MUTATION_CUT_OFF)
+#        breeding_cut_off = int(len(self.players) * SUPERIOR_BREEDING_POOL_PERC)
+#        good_players = self.players[0:survive_cut_off]
+#        bad_players = self.players[survive_cut_off:]
+#        breeding_players = self.players[0:breeding_cut_off]
+#        num_bad_take = int(len(self.players) * MUTATION_BAD_TO_KEEP)
+#
+#        print('---------------------------')
+#        for p in breeding_players:
+#            print(p.fitness)
+#
+#        for b in bad_players:
+#            b.net.modify_weights()
+#
+#        new_players = []
+#
+#        idx_bad_take = np.random.choice(np.arange(len(bad_players)), num_bad_take, replace=False)
+#
+#        for index in idx_bad_take:
+#            new_players.append(bad_players[index])
+#
+#        new_players.extend(good_players)
+#
+#        num_children_needed = len(self.players) - len(new_players)
+#
+#        while len(new_players) < len(self.players):
+#            idx_pair_breed = np.random.choice(np.arange(len(breeding_players)), 2, replace=True)
+#            child = self.breed(breeding_players[idx_pair_breed[0]], breeding_players[idx_pair_breed[1]])
+#            if random.random() < MUTATION_WEIGHT_MODIFY_CHANCE:
+#                child.net.modify_weights()
+#            new_players.append(child)
+##            idx_pair_breed = np.random.choice(np.arange(len(breeding_players)), 2, replace=False)
+##            if idx_pair_breed[0] != idx_pair_breed[1]:
+##                child = self.breed(breeding_players[idx_pair_breed[0]], breeding_players[idx_pair_breed[1]])
+##                if random.random() < MUTATION_WEIGHT_MODIFY_CHANCE:
+##                    child.net.modify_weights()
+##                new_players.append(child)
+#
+#        self.players = new_players
+#
+#        self.reset()
 
 
 
